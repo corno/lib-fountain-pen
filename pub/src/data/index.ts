552c4e70ce4f44f66d6ec313a683fde6e4d35c24
corno <corno.astn.a@schraverus.com>
2023-01-15 14:20:10 +0100

    .

--- pub/src/data/index.ts
+++
@@ -1,6 +0,0 @@
-import * as types from "../modules/main/api"
-
-export const _defaultSettings: types.TConfiguration = {
-    'newline': `\r\n`,
-    'indentation': `    `,
-}
--- pub/src/modules/main/implementations/createFountainPen.p.ts
+++ pub/src/modules/main/implementations/createFountainPen.p.ts
@@ -6,11 +6,13 @@ import * as api from "../api"
 import * as mtostring from "res-pareto-tostring"
 
 import { icreateUnboundFountainPen } from "./createUnboundFountainPen.p"
-import { _defaultSettings } from "../../../data"
 
 export const icreateFountainPen: api.CcreateFountainPen = ($d) => {
     return icreateUnboundFountainPen(
-        _defaultSettings,
+        {
+            'newline': `\r\n`,
+            'indentation': `    `,
+        },
         {
             sf_joinNestedStrings: mtostring.$a.joinNestedStrings,
             sf_getArrayAsString: mtostring.$a.getArrayAsString,
