f55aaa52844b81a0cb5fe865e89bdd5dd744ea16
Corno Schraverus <corno@schraverus.com>
2024-09-11 13:26:29 +0200

    .

--- typescript/pub/src/submodules/serialize/glossary/datatypes.generated.ts
+++
@@ -1,9 +0,0 @@
-import * as pt from 'pareto-core-types'
-
-import * as g_common from "glo-pareto-common"
-import * as g_main from "../../../main"
-import * as g_typelibrary from "../../typelibrary"
-
-export namespace N {}
-
-export namespace T {}
